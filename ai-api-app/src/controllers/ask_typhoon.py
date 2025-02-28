68a3344615959fc4539d93dc0f041733872aa386
choonewza-pro <choonewza@gmail.com>
2025-02-28 11:10:11 +0700

    add time

--- ai-api-app/src/controllers/ask_typhoon.py
+++ ai-api-app/src/controllers/ask_typhoon.py
@@ -1,3 +1,4 @@
+import time
 from fastapi import APIRouter, Query
 from src.utils.typhoon_2_assistant import Typhoon2Assistant
 
@@ -7,9 +8,16 @@ router = APIRouter()
 def ask_typhoon(
     prompt: str = Query(..., description="The prompt to ask the website"),
 ):
+    start_time = time.time()
+
     llm = Typhoon2Assistant(system_content="คุณเป็นผู้ช่วยที่เป็นมิตร ตอบคำถามได้เป็นอย่างดี")
     answer = llm.ask(prompt)
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+
     return {
         "answer": answer,
         "prompt": prompt,
+        "execution_time": execution_time
     }
--- ai-api-app/src/controllers/ask_typhoon_summary.py
+++ ai-api-app/src/controllers/ask_typhoon_summary.py
@@ -1,3 +1,4 @@
+import time
 from fastapi import APIRouter
 from pydantic import BaseModel
 from src.utils.typhoon_2_assistant import Typhoon2Assistant
@@ -9,9 +10,16 @@ class QuestionRequest(BaseModel):
 
 @router.post("/ask-typhoon-summary")
 def ask_typhoon_summary(request: QuestionRequest):
+    start_time = time.time()
+
     llm = Typhoon2Assistant(system_content="ช่วยสรุปข้อความที่รับมาให้เข้าใจง่าย สั้นกระชับ และไม่ยาวมากนัก ใน 1 paragraph")
     answer = llm.ask(request.prompt)
+    
+    end_time = time.time()
+    execution_time = end_time - start_time
+    
     return {
         "answer": answer,
         "prompt": request.prompt,
+        "execution_time": execution_time
     }
