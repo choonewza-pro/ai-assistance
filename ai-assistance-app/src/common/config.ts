4b867be069be0fe39f6853938b56681975f895ab
choonewza-pro <choonewza@gmail.com>
2025-03-07 05:00:53 +0700

    ok

--- ai-assistance-app/src/common/config.ts
+++ ai-assistance-app/src/common/config.ts
@@ -1,7 +1,7 @@
 export const appConfig = {
-    baseUrl: 'http://localhost',
-    apiBaseUrl: 'http://localhost',
-    // baseUrl:"http://122.155.209.179",
-    // apiBaseUrl: 'http://ai-api-app',
+    // baseUrl: 'http://localhost',
+    // apiBaseUrl: 'http://localhost',
+    baseUrl:"http://122.155.209.179",
+    apiBaseUrl: 'http://ai-api-app',
 }
 export default appConfig;
